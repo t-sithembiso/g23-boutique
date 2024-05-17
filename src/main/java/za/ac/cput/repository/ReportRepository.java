c3da59ffec609cf05cf6f76c27f4f7bc951df010
Brian <ramorokaob@gmail.com>
2024-05-17 14:16:28 +0200

    Report domain, factory and repository

--- src/main/java/za/ac/cput/repository/ReportRepository.java
+++ src/main/java/za/ac/cput/repository/ReportRepository.java
@@ -3,5 +3,5 @@ package za.ac.cput.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import za.ac.cput.domain.Report;
 
-public interface ReportRepository extends JpaRepository<Report, Integer> {
+public interface ReportRepository extends JpaRepository<Report, Long> {
 }
