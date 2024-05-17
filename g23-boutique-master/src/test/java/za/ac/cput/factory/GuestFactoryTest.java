8c293605635e0ff7ba3308f055afc45f26f70816
Sibusiso Kubalo <2183160382@mycput.ac.za>
2024-05-17 20:35:01 +0200

    my first commit

---
+++ g23-boutique-master/src/test/java/za/ac/cput/factory/GuestFactoryTest.java
@@ -0,0 +1,16 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.Guest;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuestFactoryTest {
+
+    @Test
+    void buildGuest() {
+    }
+
+}
