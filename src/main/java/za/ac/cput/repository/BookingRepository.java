21c2284c044d63ad4eca811832ba3d4926387a4f
Brian <ramorokaob@gmail.com>
2024-05-17 18:28:32 +0200

    Updated domain, repository and factory test

--- src/main/java/za/ac/cput/repository/BookingRepository.java
+++ src/main/java/za/ac/cput/repository/BookingRepository.java
@@ -1,4 +1,7 @@
 package za.ac.cput.repository;
 
-public interface BookingRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.domain.Housekeeping;
+
+public interface BookingRepository extends JpaRepository<Housekeeping, Long> {
 }
