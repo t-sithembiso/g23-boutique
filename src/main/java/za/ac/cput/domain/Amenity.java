a0e04b0bbab4f2b84e1515b76ce4c8b0dc03b5a1
218272510 <218272510@mycput.ac.za>
2024-05-19 19:59:21 +0200

    Please check my Amenity

--- src/main/java/za/ac/cput/domain/Amenity.java
+++ src/main/java/za/ac/cput/domain/Amenity.java
@@ -9,7 +9,7 @@ import java.util.Date;
 import java.util.Objects;
 
 @Entity
-public class Amenity {
+public class Amenity extends Cost {
     @Id
     private String amenityId;
     private String amenityName;
@@ -24,31 +24,37 @@ public class Amenity {
     @JoinColumn(name = "booked")
     private Booking booking;
 
-    public Amenity() {
-
-    }
+    protected Amenity() {}
 
     public Amenity(Builder builder) {
         this.amenityId = builder.amenityId;
         this.amenityName = builder.amenityName;
         this.amenityDescription = builder.amenityDescription;
         this.available = builder.available;
+        this.costId = builder.costId;
+        this.inventoryId = builder.inventoryId;
+        this.costDescription =builder.costDescription;
+        this.amount = builder.amount;
+        this.dateIncurred = builder.dateIncurred;
 
     }
+    public String getAmenityId() {return amenityId;}
 
-    public String getAmenityId() {
-        return amenityId;
-    }
+    public String getAmenityName() {return amenityName;}
 
-    public String getAmenityName() {
-        return amenityName;
-    }
-    public String getAmenityDescription() {
-        return amenityDescription;
-    }
-    public boolean isAvailable() {
-        return available;
-    }
+    public String getAmenityDescription() {return amenityDescription;}
+
+    public boolean isAvailable() {return available;}
+
+    public String getCostId() {return costId;}
+
+    public String getInventoryId() {return inventoryId;}
+
+    public String getCostDescription() {return costDescription;}
+
+    public Double getAmount() {return amount;}
+
+    public Date getDateIncurred() {return dateIncurred;}
 
     public Booking getBooking() {
         return booking;
@@ -78,6 +84,11 @@ public class Amenity {
     }
 
     public static class Builder {
+        public String costDescription;
+        public String inventoryId;
+        public String costId;
+        public Double amount;
+        public Date dateIncurred;
         private String amenityId;
         private String amenityName;
         private String amenityDescription;
@@ -85,23 +96,39 @@ public class Amenity {
         private Booking booking;
 
 
+        public Builder setCostDescription(String costDescription) {
+            this.costDescription = costDescription;
+            return this;
+        }
 
-
+        public Builder setInventoryId(String inventoryId) {
+            this.inventoryId = inventoryId;
+            return this;
+        }
+        public Builder setCostId(String costId) {
+            this.costId = costId;
+            return this;
+        }
+        public Builder setAmount(Double amount) {
+            this.amount = amount;
+            return this;
+        }
+        public Builder setDateIncurred(Date dateIncurred) {
+            this.dateIncurred = dateIncurred;
+            return this;
+        }
         public Builder setAmenityId(String amenityId) {
             this.amenityId = amenityId;
             return this;
         }
-
         public Builder setAmenityName(String amenityName) {
             this.amenityName = amenityName;
             return this;
         }
-
         public Builder setAmenityDescription(String amenityDescription) {
             this.amenityDescription = amenityDescription;
             return this;
         }
-
         public Builder setAvailable(boolean available) {
             this.available = available;
             return this;
@@ -111,15 +138,18 @@ public class Amenity {
             this.booking = booking;
             return this;
         }
-
         public Builder copy(Amenity amenity) {
             this.amenityId = amenity.amenityId;
             this.amenityName = amenity.amenityName;
             this.amenityDescription = amenity.amenityDescription;
             this.available = amenity.available;
+            this.inventoryId = amenity.inventoryId;
+            this.amount = amenity.amount;
+            this.costId = amenity.costId;
+            this.costDescription = amenity.costDescription;
+            this.dateIncurred = amenity.dateIncurred;
             return this;
         }
-
         public Amenity build() {
             return new Amenity(this);
         }
