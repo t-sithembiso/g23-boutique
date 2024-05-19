561e86747da8195c851f61e81e72415389dd6d79
t-sithembiso <102856482+t-sithembiso@users.noreply.github.com>
2024-05-19 06:25:58 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/service/HouseKeepingService.java
@@ -0,0 +1,42 @@
+package za.ac.cput.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Housekeeping;
+import za.ac.cput.repository.HouseKeepingRepository;
+
+import java.util.List;
+
+@Service
+public class HouseKeepingService implements IHouseKeepingService{
+
+
+    private final HouseKeepingRepository houseKeepingRepositoryObj;
+    @Autowired
+    protected HouseKeepingService(HouseKeepingRepository houseKeepingRepositoryObj){
+        this.houseKeepingRepositoryObj=houseKeepingRepositoryObj;
+
+    }
+
+
+
+    @Override
+    public List<Housekeeping> getall() {
+        return houseKeepingRepositoryObj.findAll();
+    }
+
+    @Override
+    public Housekeeping create(Housekeeping housekeeping) {
+        return houseKeepingRepositoryObj.save(housekeeping);
+    }
+
+    @Override
+    public Housekeeping update(Housekeeping housekeeping) {
+        return houseKeepingRepositoryObj.save(housekeeping);
+    }
+
+    @Override
+    public Housekeeping read(Long id) {
+        return houseKeepingRepositoryObj.findById(id).orElse(null);
+    }
+}
